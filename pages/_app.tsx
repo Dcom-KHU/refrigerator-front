261624c7f51b1a9e0d198d2322332584d1b2afa6
Ellie <dalshot7@naver.com>
2022-08-12 02:47:57 +0900

    fix : Home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
+import  '../styles/globals.css'
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
+  
         <RecoilRoot>
             <Component {...pageProps} />
         </RecoilRoot>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,25 @@
 import type { NextPage } from 'next';
+import Header from '../components/header';
+import SearchRecipe from '../components/searchrecipe';
+import TodaysRecipe from '../components/todaysrecipe';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
+
     return (
-        <>
-            <h1>:D</h1>
-        </>
-    );
+    <div>
+        <Head>
+            <title>Recipes</title>
+        </Head>
+
+        <Header /> 
+        
+        <div className="grid place-items-center">
+        <TodaysRecipe menu='menu' />
+        <SearchRecipe/>
+        </div>
+    </div>
+    )
 };
 
 export default Home;
