9a533beef3aa34fdd5d5293913266176b2bfc681
Ellie <dalshot7@naver.com>
2022-08-12 02:41:58 +0900

    feat : Search recipe

--- components/searchrecipe.tsx
+++ components/searchrecipe.tsx
@@ -11,17 +11,20 @@ import Image from 'next/image';
 const SearchRecipe:NextPage = () => {
 return (
 <> 
-   
-    <div className='mx-1 flex flex-row' >
-    
-        <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png"></img>
-        <input  type ='text' placeholder='레시피검색' />
-        <StyledButton title="검색"/>
-   
+<form>   
+    <label htmlFor= "search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Your Email</label>
+    <div className="relative">
+        <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+        </div>
+        <input type="search" id="search" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required/>
+        <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-red-200 hover:bg-red-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
     </div>
-    
+</form>
 
-    <div className='mx-1 flex flex-row ' >
+     
+        
+    <div className='my-3 flex flex-row ' >
         <StyledButton title="한식"/>
         <StyledButton title="중식"/>
         <StyledButton title="양식"/>
@@ -31,14 +34,14 @@ return (
 
     <div>
        
-    <div className="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
-        <Image src= '/food2.jpeg' width = {500}  height = {500}/>   
-            <Link href={'/detail'}>음식이름</Link>
+       <RecipeList/>
     
  
-</div>
     </div>
+
 </>
+
+
 );
 };
 
