e14e1ef87bdd08cc65cf0e88e3eb6e5140da2796
HYEOK9 <leejaehyuck9@gmail.com>
2022-08-19 12:20:05 +0900

    fix:animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
                 fadeIn: 'fadeIn 0.2s ease forwards',
                 fadeOut: 'fadeOut 0.2s ease forwards',
                 liAdd: 'liAdd 0.2s linear forwards',
-                liDel: 'liDel 0.3s ease-in forwards',
+                liDel: 'fadeOut 0.3s ease-out forwards',
             },
             keyframes: {
                 labelUp: {
@@ -29,10 +29,6 @@ module.exports = {
                 liAdd: {
                     '0%': { transform: 'translateY(-100%)' },
                     '100%': { transform: 'translateY(0)' },
-                },
-                liDel: {
-                    '0%': { transform: 'translateX(0)' },
-                    '100%': { transform: 'translateX(-100%)' },
                 },
             },
         },
